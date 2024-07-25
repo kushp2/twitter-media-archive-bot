ad5cb34092345327a78f0ea93d50d15a0d81943c
Kush Patel <kushpatel2001@gmail.com>
2024-07-25 03:02:31 -0500

    added exception for posting a tweet

--- python/twitter_bot.py
+++ python/twitter_bot.py
@@ -9,13 +9,17 @@ client_v1 = get_twitter_conn_v1()
 client_v2 = get_twitter_conn_v2()
 
 def upload_image(path):
+    # Gets the Image and formats it through client v1
     media = client_v1.simple_upload(filename=path)        
     media_id = media.media_id
 
-    # Post the tweet with the image and tweet_text
-    tweet = client_v2.create_tweet(media_ids=[media_id])
-
-    print("Success - Tweet has been posted")
+    # Post the tweet with the image through client v2
+    try:
+        tweet = client_v2.create_tweet(media_ids=[media_id])
+        print("Success - Tweet has been posted")
+    except Exception as e:
+        print("Error - Uploading Media")
+        
 
 # Delete the image file after tweeting
 #os.remove(media_path)
