7526772c7c01a9d2e0aaa8cea6d7bc5abf8e6d7d
Kush Patel <kushpatel2001@gmail.com>
2024-07-26 21:45:58 -0500

    added a function to display the whole tweets table

--- python/database.py
+++ python/database.py
@@ -1,9 +1,10 @@
-import pypyodbc as odbcv
+import pypyodbc as odbc
 import pandas as pd
 import os
 from dotenv import load_dotenv
 
 # DB login credentials
+load_dotenv()
 server = os.getenv('DB_SERVER')
 database = os.getenv('DB_NAME')
 username = os.getenv('DB_USERNAME')
@@ -11,6 +12,7 @@ password = os.getenv('DB_PASSWORD')
 connection_string = 'Driver={ODBC Driver 18 for SQL Server};Server='+ server +';Database='+ database +';Uid='+ username +';Pwd='+ password +';Encrypt=yes;TrustServerCertificate=no;Connection Timeout=120;'
 conn = odbc.connect(connection_string)
 
+# Queries
 create_table_sql = '''
 CREATE TABLE tweets (
     id INT PRIMARY KEY IDENTITY(1,1),
@@ -25,6 +27,7 @@ INSERT INTO tweets (tweet, date, images)
 VALUES (?, ?, ?);
 """
 
+display_tweets = 'SELECT * FROM tweets'
 
 def create_table():
     '''Creates a table to hold tweets'''
@@ -35,6 +38,7 @@ def create_table():
     except Exception as e:
         print(f"An error occurred: {e}")
 
+
 def insert_tweet(tweet_text, tweet_date, images_links):
     '''Adds a tweet to the database'''
     try:
@@ -43,6 +47,26 @@ def insert_tweet(tweet_text, tweet_date, images_links):
             conn.commit()
     except Exception as e:
         print(f"An error occurred: {e}")
+
+
+def display_table():
+    '''Fetches data from the tweets table and prints it.'''
+    try:
+        # Create a cursor and execute the query
+        with conn.cursor() as cursor:
+            cursor.execute(display_tweets)
+
+            # Gets the row and column data
+            rows = cursor.fetchall()
+            columns = [column[0] for column in cursor.description]
+            
+            # Create and print DataFrame to display the table
+            df = pd.DataFrame(rows, columns=columns)
+            print(df)
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
 
 def close_connection():
     '''Closes the database connection'''
