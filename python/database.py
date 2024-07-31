dafbf680572d3520e22b07d42681e5fc66e42004
Kush Patel <kushpatel2001@gmail.com>
2024-07-31 18:07:50 -0500

    Added functions for fetching a random tweet, and deleteting a tweet from the db

--- python/database.py
+++ python/database.py
@@ -12,25 +12,19 @@ password = os.getenv('DB_PASSWORD')
 connection_string = 'Driver={ODBC Driver 18 for SQL Server};Server='+ server +';Database='+ database +';Uid='+ username +';Pwd='+ password +';Encrypt=yes;TrustServerCertificate=no;Connection Timeout=120;'
 conn = odbc.connect(connection_string)
 
-# Queries
-create_table_sql = '''
-CREATE TABLE tweets (
-    id INT PRIMARY KEY IDENTITY(1,1),
-    tweet NVARCHAR(MAX),
-    date NVARCHAR(50),
-    images NVARCHAR(MAX)
-);
-'''
-
-insert_tweet_sql = """
-INSERT INTO tweets (tweet, date, images)
-VALUES (?, ?, ?);
-"""
-
-display_tweets = 'SELECT * FROM tweets'
 
 def create_table():
     '''Creates a table to hold tweets'''
+    # Query
+    create_table_sql = '''
+        CREATE TABLE tweets (
+            id INT PRIMARY KEY IDENTITY(1,1),
+            tweet NVARCHAR(MAX),
+            date NVARCHAR(50),
+            images NVARCHAR(MAX)
+        );
+    '''
+
     try:
         with conn.cursor() as cursor:
             cursor.execute(create_table_sql)
@@ -41,6 +35,12 @@ def create_table():
 
 def insert_tweet(tweet_text, tweet_date, images_links):
     '''Adds a tweet to the database'''
+    # Query
+    insert_tweet_sql = """
+        INSERT INTO tweets (tweet, date, images)
+        VALUES (?, ?, ?);
+    """
+    
     try:
         with conn.cursor() as cursor:
             cursor.execute(insert_tweet_sql, (tweet_text, tweet_date, images_links))
@@ -51,6 +51,9 @@ def insert_tweet(tweet_text, tweet_date, images_links):
 
 def display_table():
     '''Fetches data from the tweets table and prints it.'''
+    # Query
+    display_tweets = 'SELECT * FROM tweets'
+
     try:
         # Create a cursor and execute the query
         with conn.cursor() as cursor:
@@ -66,6 +69,29 @@ def display_table():
 
     except Exception as e:
         print(f"An error occurred: {e}")
+
+
+def fetch_random_tweet():
+    '''Fetches a random tweet from the database'''
+    fetch_random_tweet_sql = "SELECT TOP 1 * FROM tweets ORDER BY NEWID();"
+    cursor = conn.cursor()
+    cursor.execute(fetch_random_tweet_sql)
+    
+    # Creates a tuple, (id, tweet number, date posted, image url)
+    tweet = cursor.fetchone()
+    cursor.close()
+
+    return tweet
+
+
+def delete_tweet_by_id(tweet_id):
+    '''Deletes a tweet from the database by ID'''
+    # Query
+    delete_tweet_sql = "DELETE FROM tweets WHERE id = ?;"
+    cursor = conn.cursor()
+    cursor.execute(delete_tweet_sql, (tweet_id,))
+    conn.commit()
+    cursor.close()
 
 
 def close_connection():
